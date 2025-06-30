40abf49042fa9a170b7648146435319b474bb306
MrOscarbbx <misterioso1265@gmail.com>
2025-06-29 21:01:33 -0600

    Cambios Nuevos

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
     items.forEach(item => observer.observe(item));
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const splashScreen = document.querySelector('.splash-screen');
+    const logo = document.querySelector('.logo');
+
+    setTimeout(() => {
+        setTimeout(() => {
+            splashScreen.classList.add("display-none");
+        },0); // Espera a que la transición de opacidad termine
+    }, 1000); // Espera 3 segundos antes de ocultar la pantalla de inicio
+});
